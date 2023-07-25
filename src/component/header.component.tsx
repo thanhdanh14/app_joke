7f139c75cf2fe80533ae0c6e67fb79ff38cd4368
Danh <thanhdanh010499@gmail.com>
2023-07-25 17:28:51 +0700

    first commit

---
+++ src/component/header.component.tsx
@@ -0,0 +1,20 @@
+import logo1  from '../assets/logo1.png'
+import avatar from '../assets/avatar.png'
+export default function Header() {
+  return (
+    <>
+      <div className='flex justify-around my-2  '>
+        <h1 className=' py-1'>
+          <img src={logo1} alt='logo'/>
+        </h1>
+        <div className='flex'>
+          <div className='flex-col pt-[15px]'>
+          <h2 className='text-gray-400 w-[120px] '>Handicrafted by</h2>
+          <h2 className='flex justify-end mr-[4px]'>Jim HLS</h2>
+          </div>
+          <img src={avatar} alt='avatar'/>
+        </div>
+      </div>
+    </>
+  )
+}
